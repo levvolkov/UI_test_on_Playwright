b2f068b767369455c0291bd3539832e59641a169
Lev <03011984lev@gmail.com>
2024-07-09 00:26:20 +0300

    Initial commit

---
+++ tests/example.spec.js
@@ -0,0 +1,21 @@
+const { test, expect } = require("@playwright/test");
+const { email, password, invalidEmail, invalidPassword } = require("../user.js");
+
+test("Successfull authorization", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").fill(email);
+  await page.getByPlaceholder("Пароль").fill(password);
+  await page.getByTestId("login-submit-btn").click();
+  await expect(page.locator("h2")).toContainText("Моё обучение");
+  await page.screenshot({ path: "./screenshots/successfulAuth.png" });
+});
+
+test("Unsuccessfull authorization", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").fill(invalidEmail);
+  await page.getByPlaceholder("Пароль").fill(invalidPassword);
+  await page.getByTestId("login-submit-btn").click();
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  await expect(page.locator("text=Вы ввели неправильно логин или пароль")).toBeVisible();
+  await page.screenshot({ path: "./screenshots/unsuccessfulAuth.png" });
+});
